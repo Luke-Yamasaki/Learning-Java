8db9fcfcba92b1e2333b58c552fd501d49cbc1b6
Luke Yamasaki <lukeyamasaki@gmail.com>
2022-07-18 20:07:53 -0600

    Learning Java variables

---
+++ com/java24hours/Variable.java
@@ -0,0 +1,21 @@
+package com.java24hours;
+
+class Variable {
+    public static void main(String[] arguments) {
+        int tops;
+        float gradePointAverage;
+        char key = 'C';
+        String productName = "Larvets";
+        byte excapeKey = 27;
+        short roomNumber = 222;
+        long salary = 264_400_000;
+        boolean gameOver = false;
+        double pi = 3.14;
+        int mileage = 300;
+        int totalMileage = mileage;
+        final int TOUCHDOWN = 6;
+        final int FIELDGOAL = 3;
+        final int PAT = 1;
+
+    }
+}
