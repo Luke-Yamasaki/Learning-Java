593447030ec18bfa83686a59af2a4cfc949dcbda
Luke Yamasaki <lukeyamasaki@gmail.com>
2022-07-18 21:08:43 -0600

    Bonus questions

--- com/java24hours/Variable.java
+++ com/java24hours/Variable.java
@@ -1,38 +1,38 @@
-package com.java24hours;
+// package com.java24hours;
 
-class Variable {
-    public static void main(String[] arguments) {
-        int tops;
-        float gradePointAverage;
-        char key = 'C';
-        String productName = "Larvets";
-        byte excapeKey = 27;
-        short roomNumber = 222;
-        long salary = 264_400_000;
-        boolean gameOver = false;
-        double pi = 3.14;
-        int mileage = 300;
-        int totalMileage = mileage;
-        float f = 3.14F;
-        float d = 3.14;
-        float l = 3L;
-        final int TOUCHDOWN = 6;
-        final int FIELDGOAL = 3;
-        final int PAT = 1;
-        double weight = 205;
-        weight = weight + 10;
-        weight = weight / 3;
-        int remainder = 245 % 3;
-        int score = 20;
-        int total = 500 + (score * 12);
-        int power = 15;
-        power++;
-        int rating = 10;
-        rating--;
-        int postfixAnswer = rating++ * 100;
-        int prefixAnswer = ++rating * 100;
-        
+// class Variable {
+//     public static void main(String[] arguments) {
+//         int tops;
+//         float gradePointAverage;
+//         char key = 'C';
+//         String productName = "Larvets";
+//         byte excapeKey = 27;
+//         short roomNumber = 222;
+//         long salary = 264_400_000;
+//         boolean gameOver = false;
+//         double pi = 3.14;
+//         int mileage = 300;
+//         int totalMileage = mileage;
+//         float f = 3.14F;
+//         double d = 3.14;
+//         float l = 3L;
+//         final int TOUCHDOWN = 6;
+//         final int FIELDGOAL = 3;
+//         final int PAT = 1;
+//         double weight = 205;
+//         weight = weight + 10;
+//         weight = weight / 3;
+//         int remainder = 245 % 3;
+//         int score = 20;
+//         int total = 500 + (score * 12);
+//         int power = 15;
+//         power++;
+//         int rating = 10;
+//         rating--;
+//         int postfixAnswer = rating++ * 100;
+//         int prefixAnswer = ++rating * 100;
 
 
-    }
-}
+
+//     }
+// }
---
+++ com/java24hours/XYSquared.java
@@ -0,0 +1,10 @@
+package com.java24hours;
+
+class XYSquared {
+    public static void main(String[] arguments) {
+        int x = 11;
+        int y = 13;
+        int squared = x * x + y * y;
+        System.out.println(squared);
+    }
+}
