ad60b811404b6858eb800f74bec68ef5fc36b69c
Luke Yamasaki <lukeyamasaki@gmail.com>
2022-07-18 20:58:54 -0600

    Practicing operators in Java

--- com/java24hours/Variable.java
+++ com/java24hours/Variable.java
@@ -13,9 +13,26 @@ class Variable {
         double pi = 3.14;
         int mileage = 300;
         int totalMileage = mileage;
+        float f = 3.14F;
+        float d = 3.14;
+        float l = 3L;
         final int TOUCHDOWN = 6;
         final int FIELDGOAL = 3;
         final int PAT = 1;
+        double weight = 205;
+        weight = weight + 10;
+        weight = weight / 3;
+        int remainder = 245 % 3;
+        int score = 20;
+        int total = 500 + (score * 12);
+        int power = 15;
+        power++;
+        int rating = 10;
+        rating--;
+        int postfixAnswer = rating++ * 100;
+        int prefixAnswer = ++rating * 100;
+        
+
 
     }
 }
