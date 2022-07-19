5f067ce258b3ab8b131e0f789ec4495501dcff15
Luke Yamasaki <lukeyamasaki@gmail.com>
2022-07-18 21:36:11 -0600

    Advanced string methodsg

--- com/java24hours/StringNotes.java
+++ com/java24hours/StringNotes.java
@@ -34,5 +34,35 @@ class StringNotes {
         System.out.println("\"\'Sharknado\' is an hour and a half of your "
             + "life that you'll never get back.\nAnd you won't want to.\"\n"
             + "\t-- David Hinckley, New York Daily News");
+
+        int length = 86;
+        char rating = 'R';
+        System.out.println("Running time: " + length + " minutes");
+        System.out.println("Rated " + rating);
+
+        String searchKeywords = "";
+        searchKeywords = searchKeywords + "shark ";
+        searchKeywords = searchKeywords + "hurricane ";
+        searchKeywords = searchKeywords + "danger";
+        System.out.println(searchKeywords);
+
+        String favorite = "chainsaw";
+        String guess = "pool cue";
+        System.out.println("Is Fin's favorite weapon a " + guess + "?");
+        System.out.println("Answer: " + favorite.equals(guess));
+
+        String cinematographer = "Ben Demaree";
+        int nameLength = cinematographer.length();
+        System.out.println(nameLength);
+
+        String fin = "Ian Ziering";
+        String change = fin.toUpperCase();
+        String changeAgain = fin.toLowerCase();
+        System.out.println(change);
+        System.out.println(changeAgain);
+
+        
+
+
     }
 }
