55dfd5d057d5efda1dfd70467d9c77822b14a59c
Luke Yamasaki <lukeyamasaki@gmail.com>
2022-07-19 18:18:37 -0600

    Learned about when to use if else vs switch case

---
+++ com/java24hours/GradePractice.java
@@ -0,0 +1,108 @@
+package com.java24hours;
+
+class GradePractice {
+    public static void main(String[] arguments) {
+        int grade = 59;
+
+        // If else statements
+        if(grade >= 90) {
+            System.out.print("You are an A student! Great job!");
+        } else if(grade < 90 && grade >= 80) {
+            System.out.print("You are a B student. Doing good!");
+        } else if(grade < 80 && grade >= 70) {
+            System.out.print("You are a C student. Remember, C is the lowest passing grade.");
+        } else if(grade < 70 && grade >= 60) {
+            System.out.print("You are a D student. Let's get you some help to pass this class.");
+        } else {
+            System.out.print("You are an F student. You still have a chance to pass! Let's get you help immediately.");
+        }
+
+        // Switch case statements
+        switch (grade) {
+            case 100:
+                System.out.print("You are an A student! Great job!");
+            case 99:
+                System.out.print("You are an A student! Great job!");
+            case 98:
+                System.out.print("You are an A student! Great job!");
+            case 97:
+                System.out.print("You are an A student! Great job!");
+            case 96:
+                System.out.print("You are an A student! Great job!");
+            case 95:
+                System.out.print("You are an A student! Great job!");
+            case 94:
+                System.out.print("You are an A student! Great job!");
+            case 93:
+                System.out.print("You are an A student! Great job!");
+            case 92:
+                System.out.print("You are an A student! Great job!");
+            case 91:
+                System.out.print("You are an A student! Great job!");
+            case 90:
+                System.out.print("You are an A student! Great job!");
+            case 89:
+                System.out.print("You are an B student! Doing good!");
+            case 88:
+                System.out.print("You are an B student! Doing good!");
+            case 87:
+                System.out.print("You are an B student! Doing good!");
+            case 86:
+                System.out.print("You are an B student! Doing good!");
+            case 85:
+                System.out.print("You are an B student! Doing good!");
+            case 84:
+                System.out.print("You are an B student! Doing good!");
+            case 83:
+                System.out.print("You are an B student! Doing good!");
+            case 82:
+                System.out.print("You are an B student! Doing good!");
+            case 81:
+                System.out.print("You are an B student! Doing good!");
+            case 80:
+                System.out.print("You are an B student! Doing good!");
+            case 79:
+                System.out.print("You are an C student! Remember, C is the lowest passing grade.");
+            case 78:
+                System.out.print("You are an C student! Remember, C is the lowest passing grade.");
+            case 77:
+                System.out.print("You are an C student! Remember, C is the lowest passing grade.");
+            case 76:
+                System.out.print("You are an C student! Remember, C is the lowest passing grade.");
+            case 75:
+                System.out.print("You are an C student! Remember, C is the lowest passing grade.");
+            case 74:
+                System.out.print("You are an C student! Remember, C is the lowest passing grade.");
+            case 73:
+                System.out.print("You are an C student! Remember, C is the lowest passing grade.");
+            case 72:
+                System.out.print("You are an C student! Remember, C is the lowest passing grade.");
+            case 71:
+                System.out.print("You are an C student! Remember, C is the lowest passing grade.");
+            case 70:
+                System.out.print("You are an C student! Remember, C is the lowest passing grade.");
+            case 69:
+                System.out.print("You are a D student. Let's get you some help to pass this class.");
+            case 68:
+                System.out.print("You are a D student. Let's get you some help to pass this class.");
+            case 67:
+                System.out.print("You are a D student. Let's get you some help to pass this class.");
+            case 66:
+                System.out.print("You are a D student. Let's get you some help to pass this class.");
+            case 65:
+                System.out.print("You are a D student. Let's get you some help to pass this class.");
+            case 64:
+                System.out.print("You are a D student. Let's get you some help to pass this class.");
+            case 63:
+                System.out.print("You are a D student. Let's get you some help to pass this class.");
+            case 62:
+                System.out.print("You are a D student. Let's get you some help to pass this class.");
+            case 61:
+                System.out.print("You are a D student. Let's get you some help to pass this class.");
+            case 60:
+                System.out.print("You are a D student. Let's get you some help to pass this class.");
+            default:
+                System.out.print("You are an F student. You still have a chance to pass! Let's get you help immediately.");
+        }
+    }
+}
