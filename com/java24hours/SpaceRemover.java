a4b04c1cfdd12199bb0040ca80e6bd9c5e205357
Luke Yamasaki <lukeyamasaki@gmail.com>
2022-07-29 11:22:12 -0600

    Notes about arrays

---
+++ com/java24hours/SpaceRemover.java
@@ -0,0 +1,36 @@
+package com.java24hours;
+
+class SpaceRemover {
+    public static void main(String[] arguments) {
+
+    }
+}
+
+// Creating arrays
+// String[] badKid;
+// int[] reindeerWeight;
+// boolean[] hostileAirTravelNations;
+// int[] elfSeniority = new int[250]; *This creates an array from 0 to 249
+// String[] reindeerNames = { "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donder", "Blitzen"};
+
+// String[] reindeerNames = new String[8];
+// reindeerNames[0] = "Dasher";
+// reindeerNames[1] = "Dancer";
+// reindeerNames[2] = "Prancer";
+// reindeerNames[3] = "Vixen";
+// reindeerNames[4] = "Comet";
+// reindeerNames[5] = "Cupid";
+// reindeerNames[6] = "Donder";
+// reindeerNames[7] = "Blitzen";
+// After and array gets created in Jave, you can't add more items
+
+// elfSeniority[193] += 1;
+// niceChild[9428] = "Eli";
+// currentNation = 413;
+// if (hostileAirTravelNations[currentNation] == true) {
+//     sendGiftByMail();
+// }
+
+// String[] topGifts = new String[10];
+// If you try to key into topGifts[10], you will get an ArrayIndexOutOfBoundsException error message
+
