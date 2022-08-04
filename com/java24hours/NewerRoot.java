0fdc9359e16b7fd4794f2d62c08c23e88f6083f2
Luke Yamasaki <lukeyamasaki@gmail.com>
2022-08-04 17:53:05 -0600

    Adding variable casting notes

---
+++ com/java24hours/NewerRoot.java
@@ -0,0 +1,35 @@
+package com.java24hours;
+
+class NewerRoot {
+
+}
+
+//Instances in Java
+// class Modem {
+//     int speed;
+//     public void displaySpeed() {
+//         System.out.println("Speed: " + speed);
+//     }
+// }
+
+// Modem device = new Modem();
+// device.speed = 28800;
+// device.displaySpeed();
+
+// Inheritance is the same in Java
+// public class ErrorModem extends Modem {...new program}
+
+// Casting is converting a source variable to a new destination variable
+// Source variables will be a different type than the destination variable
+// i.e.
+// float source = 7.06F;
+// int destination = (int) source;
+// * You have to wrap the new type in (), like (int)
+
+// Most of the time destination variables will have more than enough space
+// Sometimes the destination will be smaller like integer to byte
+// You have to explicitly cast it
+// i.e.
+// int xNum = 103;
+// byte val = (byte) xNum;
+
