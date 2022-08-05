f85cebb2bfbe06b5bb701ed7e9ff6981b0b66266
Luke Yamasaki <lukeyamasaki@gmail.com>
2022-08-05 11:09:09 -0600

    Learned how to take a string and cast it to an integer

--- com/java24hours/NewerRoot.java
+++ com/java24hours/NewerRoot.java
@@ -1,7 +1,17 @@
 package com.java24hours;
 
 class NewerRoot {
-
+    public static void main(String[] arguments) {
+        int number = 100;
+        if (arguments.length > 0) {
+            number = Integer.parseInt(arguments[0]);
+        }
+        System.out.println("The square root of " 
+        + number
+        + " is "
+        + Math.sqrt(number)
+        );
+    }
 }
 
 //Instances in Java
@@ -32,4 +42,25 @@ class NewerRoot {
 // i.e.
 // int xNum = 103;
 // byte val = (byte) xNum;
+// Also,
+// Java will automatically change the value to fit in a range if it is too large
 
+// Sometime you don't need to cast at all
+// char can be used like int
+// int can be used like long
+// anything can be used as double
+
+// You can cast an object into other objects when they are related by inheritance
+// Example
+// public void paintComponent(Graphics comp) {
+//     Graphics2D comp2D = (Graphics2D) comp;
+// }
+// This casts a Graphics object called comp into a Graphics2D object
+
+// You can't cast an object to a variable and vice versa
+// But there are leading capital variable classes that allow you to do it
+// Boolean, Byte, Character, Double, Float, Integer, Long and Short
+// Integer suffix = new Integer(5309);
+// int newSuffix = suffix.intValue();
+// String count = "25";
+// int myCount = Integer.parseInt(count);
